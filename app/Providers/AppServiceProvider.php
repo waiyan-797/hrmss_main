d91293d21ce7b6b9e3e1e0919235f6fec3643780
SawHtutNaing <sawhtutnaing2004@gmail.com>
2024-11-29 14:03:11 +0630

    add pyi daung su for all

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use PhpOffice\PhpWord\Settings;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Settings::setDefaultFontName('Pyidaungsu'); // Replace 'Arial' with your desired font
+        
         Model::unguard();
     }
 }
