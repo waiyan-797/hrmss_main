b95436be3c009bf17eb793e837983a7f5b0cd593
yinmonwai <yinmonwai078@gmail.com>
2024-12-09 18:02:12 +0630

    Edit change

--- resources/views/pdf_reports/leave_nuber_percent_report3.blade.php
+++ resources/views/pdf_reports/leave_nuber_percent_report3.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Leave 3</title>
+    {{-- dskfjdg --}}
     <style type="text/css">
         page{
             background: white;
