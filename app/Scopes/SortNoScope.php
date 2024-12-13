aa82d7d0d14923a769a65b2ec96b4e4c4a932674
yinmonwai <yinmonwai078@gmail.com>
2024-12-13 10:25:31 +0630

    Edit fix error

--- app/Scopes/SortNoScope.php
+++ app/Scopes/SortNoScope.php
@@ -2,15 +2,28 @@
 
 namespace App\Scopes;
 
+use App\Livewire\Rank\Rank;
+use App\Models\Division;
+use App\Models\Rank as ModelsRank;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use PhpParser\Node\Stmt\If_;
 
 class SortNoScope implements Scope
-{
+{   
     public function apply(Builder $builder, Model $model)
     {
-        // $builder->orderBy('sort_no');
-        $builder->where('is_dica',true)->orderBy('sort_no');
+        if($model instanceof  ModelsRank){
+            $builder->where('is_dica',true) ;
+           
+        }
+
+
+        $builder->orderBy('sort_no');
+        
+        
+       
+       
     }
 }
