c8eb960d73056431c767bf4c5a0cb24397807b3e
ShineLynnAung <shine24lynn@gmail.com>
2025-03-14 15:59:28 +0630

    side nav dropdown ui changes

--- resources/views/livewire/side-nav-drop-down.blade.php
+++ resources/views/livewire/side-nav-drop-down.blade.php
@@ -1,5 +1,4 @@
 <div x-data="{ openGroup: null, open: false }">
-    <!-- Top-level button for the menu -->
     <button @click="open = !open" type="button" class="{{ collect($lists)->contains(fn($list) => request()->routeIs($list['route_name'])) ? 'bg-white/90 text-green-700' : 'text-white' }} flex items-center p-2 w-full text-base font-semibold rounded-lg transition duration-75 group hover:bg-gray-100 hover:text-green-700">
         {!! $icon !!}
         <span class="flex-1 ml-3 text-left whitespace-nowrap font-arial text-sm">{{ $label }}</span>
@@ -15,16 +14,10 @@
         
         @foreach ($grouped as $group => $items)
             @if ($group !== '')
-                <!-- If group is not empty, display group dropdown header -->
                 <li>
-                    <button @click="openGroup === '{{ $group }}' ? openGroup = null : openGroup = '{{ $group }}'" type="button" class="text-white flex items-center p-2 w-full text-base font-semibold rounded-lg transition duration-75 group hover:bg-gray-100 hover:text-green-700">
-                        <span class="flex-1 ml-3 text-left whitespace-nowrap font-arial text-sm">{{ $group }}</span>
-                        <svg :class="{ 'rotate-180': openGroup === '{{ $group }}' }" aria-hidden="true" class="w-5 h-5 transform transition-transform" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                        </svg>
-                    </button>
+                    
 
-                    <ul x-show="openGroup === '{{ $group }}'" x-cloak x-transition class="py-2 space-y-2 font-arial text-sm text-green-700 pl-6">
+                    <ul x-show="{{ $group }}" x-cloak x-transition class="py-2 space-y-2 font-arial text-sm text-green-700 pl-6">
                         @foreach ($items as $item)
                             @if ($item['route_name'] == 'sperator')
                                 <div class="bg-slate-600 h-[1px] w-full"></div>
@@ -34,6 +27,7 @@
                                 </li>
                             @endif
                         @endforeach
+                        <hr>
                     </ul>
                 </li>
             @else
